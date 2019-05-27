2375f7881532b0997320db6b9b8756ed44b949ab
Luciano Soares <lpsoares@gmail.com>
2019-05-27 16:18:08 -0300

    reporte por e-mail com áreas de interesse dos alunos

--- projetos/views.py
+++ projetos/views.py
@@ -36,6 +36,34 @@ def create_message(aluno):
         for o in Opcao.objects.filter(aluno=aluno):
             message += ("&nbsp;"*4)+"<li>"+o.projeto.titulo+" ("+o.projeto.empresa.nome_empresa+")</li>\n"
         message += '</ul>'
+        message += '<br><br>\n\n'
+        message += '&nbsp;&nbsp;Suas áreas de interesse são:<br>\n'
+        message += '<ul>'
+
+        if aluno.inovacao_social: message += (("&nbsp;"*4)+"<li>Inovação Social</li>\n") 
+        if aluno.ciencia_dos_dados: message += (("&nbsp;"*4)+"<li>Ciência de Dados</li>\n")
+        if aluno.modelagem_3D: message += (("&nbsp;"*4)+"<li>Modelagem 3D</li>\n")
+        if aluno.manufatura: message += (("&nbsp;"*4)+"<li>Manufatura</li>\n")
+        if aluno.resistencia_dos_materiais: message += (("&nbsp;"*4)+"<li>Resistência dos Materiais</li>\n")
+        if aluno.modelagem_de_sistemas: message += (("&nbsp;"*4)+"<li>Modelagem de Sistemas</li>\n")
+        if aluno.controle_e_automacao: message += (("&nbsp;"*4)+"<li>Controle e Automação</li>\n")
+        if aluno.termodinamica: message += (("&nbsp;"*4)+"<li>Termodinâmica</li>\n")
+        if aluno.fluidodinamica: message += (("&nbsp;"*4)+"<li>Fluidodinâmica</li>\n")
+        if aluno.eletronica_digital: message += (("&nbsp;"*4)+"<li>Eletrônica Digital</li>\n")
+        if aluno.programacao: message += (("&nbsp;"*4)+"<li>Programação</li>\n")
+        if aluno.inteligencia_artificial: message += (("&nbsp;"*4)+"<li>Inteligência Artificial</li>\n")
+        if aluno.banco_de_dados: message += (("&nbsp;"*4)+"<li>Banco de Dados</li>\n")
+        if aluno.computacao_em_nuvem: message += (("&nbsp;"*4)+"<li>Computação em Nuvem</li>\n")
+        if aluno.visao_computacional: message += (("&nbsp;"*4)+"<li>Visão Computacional</li>\n")
+        if aluno.computacao_de_alto_desempenho: message += (("&nbsp;"*4)+"<li>Computação de Alto Desempenho</li>\n")
+        if aluno.robotica: message += (("&nbsp;"*4)+"<li>Robótica</li>\n")
+        if aluno.realidade_virtual_aumentada: message += (("&nbsp;"*4)+"<li>Realidade Virtual e Aumentada</li>\n")
+        if aluno.protocolos_de_comunicacao: message += (("&nbsp;"*4)+"<li>Protocolos de Comunicação</li>\n")
+        if aluno.eficiencia_energetica: message += (("&nbsp;"*4)+"<li>Eficiência Energética</li>\n")
+        if aluno.administracao_economia_financas: message += (("&nbsp;"*4)+"<li>Administração, Economia e Finanças</li>\n")
+
+        message += '</ul>'
+        message += '<br>\n'
         message += '<br>\n'+("&nbsp;"*12)+"atenciosamente, comitê PFE"
         message += '&nbsp;<br>\n'
         message += '&nbsp;<br>\n'
