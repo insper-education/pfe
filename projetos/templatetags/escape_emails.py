1aa74ae83f409fbb0a086c5e4b117da232865d0c
Luciano Soares <lpsoares@gmail.com>
2019-11-28 10:26:02 -0300

    Eliminado problema de e comercial em linha de URL.

---
+++ projetos/templatetags/escape_emails.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+"""
+Desenvolvido para o Projeto Final de Engenharia
+Autor: Luciano Pereira Soares <lpsoares@insper.edu.br>
+Data: 28 de Novembro de 2019
+"""
+
+from urllib import parse
+
+from django import template
+register = template.Library()
+
+@register.filter
+def parse_quote(value):
+    """Remove e-comercial e outros simbolos que podem comprometer a montagem da URL."""
+    return parse.quote(value)
