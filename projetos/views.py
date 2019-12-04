9a1401463d295dcc0e957d907e7b201e73fc01ac
Luciano Soares <lpsoares@gmail.com>
2019-12-04 13:59:34 -0300

    Novo algoritmo para formar grupos. Ainda incompleto.

--- projetos/views.py
+++ projetos/views.py
@@ -9,6 +9,7 @@ import os
 import datetime
 import re           #regular expression (para o import)
 import csv
+import sys
 
 from io import BytesIO # Para gerar o PDF
 import dateutil.parser
@@ -190,14 +191,15 @@ def get_opcao(numb, opcoes, min_group, max_group, projetos_ajustados):
         opcoesp_alunos = opcoesp.filter(aluno__user__tipo_de_usuario=1)
         opcoesp_validas = opcoesp_alunos.filter(aluno__anoPFE=configuracao.ano).\
                                          filter(aluno__semestrePFE=configuracao.semestre)
-        if len(opcoesp_validas) >= min_group: #Checa se projeto tem numero minimo de aplicantes
+    
+        #if len(opcoesp_validas) >= min_group: #Checa se projeto tem numero minimo de aplicantes
             # checa se alunos no projeto ja tem CR maior dos que ja estao no momento no projeto
-            crh = 0
-            for optv in projetos_ajustados[opcao.projeto]:
-                if optv.aluno.cr > opcao.aluno.cr:
-                    crh += 1 #conta cada aluno com cr maior que o aluno sendo alocado
-            if crh < max_group:
-                break #se tudo certo retorna esse projeto mesmo
+        crh = 0
+        for optv in projetos_ajustados[opcao.projeto]:
+            if optv.aluno.cr > opcao.aluno.cr:
+                crh += 1 #conta cada aluno com cr maior que o aluno sendo alocado
+        if crh < max_group:
+            break #se tudo certo retorna esse projeto mesmo
         # Nao achou tentando outra opcao
         numbx = get_next_opcao(numb, opcoes)
         if numbx != 0:
@@ -206,6 +208,122 @@ def get_opcao(numb, opcoes, min_group, max_group, projetos_ajustados):
         else:  # caso nao encontre mais nenhuma opcao valida
             return None
     return opcao
+
+def limita_grupo(max_group, ano, semestre, projetos_ajustados):
+    """Removendo alunos de grupos superlotados."""
+    """max_group: quantidade máxima de alunos por grupo"""
+
+    pref_pri_cr = 0.1  #talvez remover
+
+    balanceado = True
+    balanceado_max = False
+    count = 200 # Numero máximo de iterações, para não travar
+    while (not balanceado_max) and (count > 0):
+        count -= 1 # para nao correr o risco de um loop infinito
+        balanceado_max = True
+        for projeto, ops in projetos_ajustados.items():
+            if len(ops) > max_group: # Checa se projeto esta superlotado
+                remove_opcao = None
+                for option in ops:
+                    if option.prioridade < 5: #Nao move se prioridade < que 5 (REVER)
+                        if remove_opcao is None:
+                            remove_opcao = option
+                        elif (\
+                option.aluno.cr * (1-((option.prioridade-1)*pref_pri_cr))) \
+                < (remove_opcao.aluno.cr * (1-((remove_opcao.prioridade-1) * pref_pri_cr))):
+                            remove_opcao = option
+                if remove_opcao is not None:
+                    opcoes = Opcao.objects.filter(aluno=remove_opcao.aluno).\
+                                            filter(projeto__ano=ano).\
+                                            filter(projeto__semestre=semestre)
+                    next_op = get_next_opcao(remove_opcao.prioridade, opcoes)
+                    if next_op != 0:
+                        #busca nas opcoes do aluno
+                        #op2 = get_opcao(next_op, opcoes, min_group,
+                        op2 = get_opcao(next_op, opcoes, 3,
+                                        max_group, projetos_ajustados)
+                        if op2: #op2 != None
+                            balanceado_max = False
+                            balanceado = False
+                            menor_grupo = 1
+                            projetos_ajustados[projeto].remove(remove_opcao)
+                            projetos_ajustados[op2.projeto].append(op2)
+                            print("Movendo(a) "+remove_opcao.aluno.user.first_name.lower()\
+                            +" (DE): "+projeto.titulo+" (PARA):"+op2.projeto.titulo)
+    return balanceado
+
+def desmonta_grupo(min_group, ano, semestre, projetos_ajustados):
+    """Realocando alunos de grupos muito pequenos (um aluno por vez)."""
+    
+    #menor_grupo = 1 # usado para elimnar primeiro grupos de 1, depois de 2, etc
+
+
+    remove_opcao = None
+    remove_projeto = None
+    menor_opcao = sys.maxsize
+
+    # identifica projeto potencial para desmontar
+    for projeto, ops in projetos_ajustados.items():
+        menor_opcao_tmp = 0
+        if ops and (len(ops) < min_group):
+            opcoes = Opcao.objects.filter(projeto=projeto)
+            for opt in opcoes:
+                if opt.prioridade <= 5:
+                    menor_opcao_tmp += (6-opt.prioridade)**2 #projetos com prioridade 1 tem mais chances
+            if menor_opcao_tmp < menor_opcao:
+                remove_projeto = projeto
+                menor_opcao = menor_opcao_tmp
+    #print(remove_projeto)
+    
+    if remove_projeto:
+        for remove_opcao in projetos_ajustados[remove_projeto]:
+            opcoes = Opcao.objects.filter(aluno=remove_opcao.aluno).\
+                                    filter(projeto__ano=ano).\
+                                    filter(projeto__semestre=semestre)
+            next_op = get_next_opcao(remove_opcao.prioridade, opcoes)
+            if next_op != 0:
+                #busca nas opcoes do aluno
+                #op2 = get_opcao(next_op, opcoes, min_group, max_group, projetos_ajustados)
+                op2 = get_opcao(next_op, opcoes, min_group, 5, projetos_ajustados)
+                if op2: #op2 != None:
+                    balanceado = False
+                    menor_grupo = 1
+                    projetos_ajustados[remove_projeto].remove(remove_opcao)
+                    projetos_ajustados[op2.projeto].append(op2)
+                    print("Movendo(b) "+remove_opcao.aluno.user.first_name.lower()+\
+                     " (DE): "+remove_projeto.titulo+" (PARA):"+op2.projeto.titulo)
+ 
+    # ANTIGO
+    """
+    for projeto, ops in projetos_ajustados.items():
+        remove_opcao = None
+        remove_projeto = None
+        if ops and (len(ops) <= menor_grupo): #(len(ops) > 0)
+            for option in ops:
+                if remove_opcao is None:
+                    if option.prioridade < 5:  #Não tirar aluno com prioridade > que 5
+                        remove_opcao = option
+                        remove_projeto = projeto
+                elif(option.aluno.cr < remove_opcao.aluno.cr) and\
+                        (option.prioridade < 5):
+                    remove_opcao = option
+                    remove_projeto = projeto
+        if remove_opcao is not None:
+            opcoes = Opcao.objects.filter(aluno=remove_opcao.aluno).\
+                                    filter(projeto__ano=configuracao.ano).\
+                                    filter(projeto__semestre=configuracao.semestre)
+            next_op = get_next_opcao(remove_opcao.prioridade, opcoes)
+            if next_op != 0:
+                #busca nas opcoes do aluno
+                op2 = get_opcao(next_op, opcoes, min_group, max_group, projetos_ajustados)
+                if op2: #op2 != None:
+                    balanceado = False
+                    menor_grupo = 1
+                    projetos_ajustados[remove_projeto].remove(remove_opcao)
+                    projetos_ajustados[op2.projeto].append(op2)
+                    #print("Movendo(b) "+remove_opcao.aluno.user.first_name.lower()+\
+                    # " (DE): "+remove_projeto.titulo+" (PARA):"+op2.projeto.titulo)
+    """
 
 @login_required
 @permission_required('users.altera_professor', login_url='/projetos/')
@@ -231,8 +349,7 @@ def propor(request):
                                filter(anoPFE=configuracao.ano).\
                                filter(semestrePFE=configuracao.semestre)
 
-        #Checa se o CR de todos os alunos esta coreto
-        for aluno in alunos:
+        for aluno in alunos: #Checa se o CR de todos os alunos esta coreto
             if aluno.cr < 5.0:
                 return HttpResponse("Aluno: "+aluno.user.first_name+" "+aluno.user.last_name+\
                                     " ("+aluno.user.username+') com CR = '+str(aluno.cr))
@@ -253,84 +370,31 @@ def propor(request):
 
         #Posiciona os alunos nas suas melhores opcoes sem estourar o tamanho do grupo
         balanceado = False
-        count = 200
-        menor_grupo = 1 # usado para elimnar primeiro grupos de 1, depois de 2, etc
+        #count = 200
+        count = 8
         while (not balanceado) and (count > 0):
-            balanceado = True
-            balanceado_max = False
-            # Removendo alunos de grupos superlotados
-            while (not balanceado_max) and (count > 0):
-                count -= 1 # para nao correr o risco de um loop infinito
-                balanceado_max = True
-                for projeto, ops in projetos_ajustados.items():
-                    if len(ops) > max_group: # Checa se projeto esta superlotado
-                        remove_opcao = None
-                        for option in ops:
-                            if option.prioridade < 5: #Nao move se prioridade < que 5 (REVER)
-                                if remove_opcao is None:
-                                    remove_opcao = option
-                                elif (\
-                        option.aluno.cr * (1-((option.prioridade-1)*pref_pri_cr))) \
-                        < (remove_opcao.aluno.cr * (1-((remove_opcao.prioridade-1) * pref_pri_cr))):
-                                    remove_opcao = option
-                        if remove_opcao is not None:
-                            opcoes = Opcao.objects.filter(aluno=remove_opcao.aluno).\
-                                                   filter(projeto__ano=configuracao.ano).\
-                                                   filter(projeto__semestre=configuracao.semestre)
-                            next_op = get_next_opcao(remove_opcao.prioridade, opcoes)
-                            if next_op != 0:
-                                #busca nas opcoes do aluno
-                                op2 = get_opcao(next_op, opcoes, min_group,
-                                                max_group, projetos_ajustados)
-                                if op2: #op2 != None
-                                    balanceado_max = False
-                                    balanceado = False
-                                    menor_grupo = 1
-                                    projetos_ajustados[projeto].remove(remove_opcao)
-                                    projetos_ajustados[op2.projeto].append(op2)
-                                    #print("Movendo(a) "+remove_opcao.aluno.user.first_name.lower()\
-                                    #+" (DE): "+pr.titulo+" (PARA):"+op2.projeto.titulo)
+            #balanceado = True
+            balanceado = False
+            
+            limita_grupo(max_group, configuracao.ano, configuracao.semestre, projetos_ajustados)
+            desmonta_grupo(min_group, configuracao.ano, configuracao.semestre, projetos_ajustados)
 
-            # Realocando alunos de grupos muito pequenos (um aluno por vez)
-            for projeto, ops in projetos_ajustados.items():
-                remove_opcao = None
-                remove_projeto = None
-                if ops and (len(ops) <= menor_grupo): #(len(ops) > 0)
-                    for option in ops:
-                        if remove_opcao is None:
-                            if option.prioridade < 5:  #Não tirar aluno com prioridade > que 5
-                                remove_opcao = option
-                                remove_projeto = projeto
-                        elif(option.aluno.cr < remove_opcao.aluno.cr) and\
-                                (option.prioridade < 5):
-                            remove_opcao = option
-                            remove_projeto = projeto
-                if remove_opcao is not None:
-                    opcoes = Opcao.objects.filter(aluno=remove_opcao.aluno).\
-                                           filter(projeto__ano=configuracao.ano).\
-                                           filter(projeto__semestre=configuracao.semestre)
-                    next_op = get_next_opcao(remove_opcao.prioridade, opcoes)
-                    if next_op != 0:
-                        #busca nas opcoes do aluno
-                        op2 = get_opcao(next_op, opcoes, min_group, max_group, projetos_ajustados)
-                        if op2: #op2 != None:
-                            balanceado = False
-                            menor_grupo = 1
-                            projetos_ajustados[remove_projeto].remove(remove_opcao)
-                            projetos_ajustados[op2.projeto].append(op2)
-                            #print("Movendo(b) "+remove_opcao.aluno.user.first_name.lower()+\
-                            # " (DE): "+remove_projeto.titulo+" (PARA):"+op2.projeto.titulo)
+            # Próxima estapa seria puxar de volta os alunos que ficaram muito para frente nas opções
 
-            if (menor_grupo < min_group) and balanceado: #caso todos grupos com menor_grupo ja foram
-                menor_grupo += 1
-                balanceado = False
+            count -= 1
+
+
+        """
+        if (menor_grupo < min_group) and balanceado: #caso todos grupos com menor_grupo ja foram
+            menor_grupo += 1
+            balanceado = False
+        """
 
         #Cria lista para enviar para o template html
         for projeto, ops in projetos_ajustados.items():
             if ops: #len(ops) > 0
                 projeto_list.append(projeto)
                 opcoes_list.append(ops)
-        mylist = zip(projeto_list, opcoes_list)
     else:
         for projeto in projetos:
             opcoes = Opcao.objects.filter(projeto=projeto)
@@ -341,7 +405,8 @@ def propor(request):
             if opcoes1: #len(opcoes1) > 0
                 projeto_list.append(projeto)
                 opcoes_list.append(opcoes1)
-        mylist = zip(projeto_list, opcoes_list)
+    
+    mylist = zip(projeto_list, opcoes_list)
     context = {
         'mylist': mylist,
         'length': len(projeto_list),
