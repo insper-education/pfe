6de4a9726ca4b83d60bcd1761f08900d9e154305
Luciano Soares <lpsoares@gmail.com>
2020-07-27 10:21:18 -0300

    coletar sempre arquivos estaticos

--- startserver.sh
+++ startserver.sh
@@ -4,7 +4,8 @@ sudo chmod a+w pfe.log
 celery -A pfe worker -l info &
 celery -A pfe beat -l info &
 sudo python3 manage.py makemigrations
-sudo python3 manage.py migrate 
+sudo python3 manage.py migrate
+sudo python3 manage.py collectstatic --no-input
 #sudo python3 manage.py runserver 0.0.0.0:80 &
 sudo systemctl start apache2
 #tail -f /var/log/apache2/error.log
