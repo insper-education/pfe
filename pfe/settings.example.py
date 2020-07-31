79e8daed780feb6c6b962b007392bba075cb6749
Luciano Soares <lpsoares@gmail.com>
2020-07-31 18:16:47 -0300

    atualizando settings de exemplo

--- pfe/settings.example.py
+++ pfe/settings.example.py
@@ -81,9 +81,18 @@ WSGI_APPLICATION = 'pfe.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        # 'ENGINE': 'django.db.backends.sqlite3',
+        # 'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    
+        'ENGINE': 'django.db.backends.XXX',
+        'NAME': 'XXX',
+        'USER': 'XXX',
+        'PASSWORD': 'XXX',
+        'HOST': '127.0.0.1',
+        'PORT': '5432',
+    
     }
+    
 }
 
 
