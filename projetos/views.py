2b9517a8c7112fb8a03c355354c841709be1e034
Luciano Soares <lpsoares@gmail.com>
2021-04-16 19:16:14 -0300

    teste de streaming

--- projetos/views.py
+++ projetos/views.py
@@ -284,6 +284,75 @@ def projetos_fechados(request):
     return render(request, 'projetos/projetos_fechados.html', context)
 
 
+
+
+import os
+import re
+import mimetypes
+from wsgiref.util import FileWrapper
+
+from django.http.response import StreamingHttpResponse
+
+
+range_re = re.compile(r'bytes\s*=\s*(\d+)\s*-\s*(\d*)', re.I)
+
+
+class RangeFileWrapper(object):
+    def __init__(self, filelike, blksize=8192, offset=0, length=None):
+        self.filelike = filelike
+        self.filelike.seek(offset, os.SEEK_SET)
+        self.remaining = length
+        self.blksize = blksize
+
+    def close(self):
+        if hasattr(self.filelike, 'close'):
+            self.filelike.close()
+
+    def __iter__(self):
+        return self
+
+    def __next__(self):
+        if self.remaining is None:
+            # If remaining is None, we're reading the entire file.
+            data = self.filelike.read(self.blksize)
+            if data:
+                return data
+            raise StopIteration()
+        else:
+            if self.remaining <= 0:
+                raise StopIteration()
+            data = self.filelike.read(min(self.remaining, self.blksize))
+            if not data:
+                raise StopIteration()
+            self.remaining -= len(data)
+            return data
+
+
+def stream_video(request, path):
+    range_header = request.META.get('HTTP_RANGE', '').strip()
+    range_match = range_re.match(range_header)
+    size = os.path.getsize(path)
+    content_type, encoding = mimetypes.guess_type(path)
+    content_type = content_type or 'application/octet-stream'
+    if range_match:
+        first_byte, last_byte = range_match.groups()
+        first_byte = int(first_byte) if first_byte else 0
+        last_byte = int(last_byte) if last_byte else size - 1
+        if last_byte >= size:
+            last_byte = size - 1
+        length = last_byte - first_byte + 1
+        resp = StreamingHttpResponse(RangeFileWrapper(open(path, 'rb'), offset=first_byte, length=length), status=206, content_type=content_type)
+        resp['Content-Length'] = str(length)
+        resp['Content-Range'] = 'bytes %s-%s/%s' % (first_byte, last_byte, size)
+    else:
+        resp = StreamingHttpResponse(FileWrapper(open(path, 'rb')), content_type=content_type)
+        resp['Content-Length'] = str(size)
+    resp['Accept-Ranges'] = 'bytes'
+    return resp
+
+
+
+
 def get_response(file, path):
     """Checa extensão do arquivo e retorna HttpRensponse corespondente."""
     # Exemplos:
@@ -325,6 +394,7 @@ def carrega_arquivo(request, local_path, path):
                 return render(request, 'generic.html', context=context)
 
         with open(file_path, 'rb') as file:
+            return stream_video(request, file.name)
             response = get_response(file, path)
             if not response:
                 mensagem = "Erro ao carregar arquivo (formato não suportado)."
