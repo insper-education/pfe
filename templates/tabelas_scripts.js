c44c1dfb738a5afbb0aad36ed0165b472c836fd1
Luciano Soares <lpsoares@gmail.com>
2021-05-17 20:09:01 -0300

    removendo caracteres estranhos da datatable

--- templates/tabelas_scripts.js
+++ templates/tabelas_scripts.js
@@ -4,9 +4,74 @@
     Data: 30 de Janeiro de 2021
 {% endcomment %}
 
+// Atualizar com: $.fn.dataTables.Buttons.stripData
+function stripData( str ) {
+    
+    console.log(str);
+	if ( typeof str !== 'string' ) {
+		return str;
+	}
+
+	// Always remove script tags
+	str = str.replace( /<script\b[^<]*(?:(?!<\/script>)<[^<]*)*<\/script>/gi, '' );
+
+	// Always remove comments
+	str = str.replace( /<!\-\-.*?\-\->/g, '' );
+
+    str = str.replace( /&nbsp;/g, ' ' );
+    str = str.replace( /🔗/g, '' );
+
+    str = str.replace( /<[^>]*>/g, '' );
+	str = str.replace( /^\s+|\s+$/g, '' );
+	str = str.replace( /\n/g, ' ' );
+
+	// if ( config.decodeEntities ) {
+	// 	_exportTextarea.innerHTML = str;
+	// 	str = _exportTextarea.value;
+	// }
+
+	return str;
+};
+
+var buttonCommon = {
+    exportOptions: {
+        format: {
+            body: function ( data, row, column, node ) {
+                return stripData(data);
+            }
+        },
+        columns: ':visible'
+    }
+};
+
 var table = $('#{{tabela}}Table').DataTable( {
     dom: "<'row mr-1'<'col-md-6'><'col-md-6 d-flex flex-row-reverse'f>>t<'row'<'col-md-6'i><'col-md-6'p>><'row'<'col-sm'><'col-md'><'col-md text-right'l>>",
-    buttons: [ 'copy', 'csv', 'excel', 'pdf', 'colvis' ],
+    buttons: [ 
+        
+        $.extend( true, {}, buttonCommon, {
+            extend: 'copy'
+        } ),
+
+        $.extend( true, {}, buttonCommon, {
+            extend: 'csv',
+            title: 'Projeto Final de Engenharia',
+        } ),
+
+        $.extend( true, {}, buttonCommon, {
+            extend: 'excel',
+            title: 'Projeto Final de Engenharia',
+        } ),
+
+        $.extend( true, {}, buttonCommon, {
+            extend: 'pdf',
+            title: 'Projeto Final de Engenharia',
+            orientation: 'landscape',
+            pageSize: 'A4',
+            text: '<spam class="fa fa-file-pdf-o">PDF</spam>',
+        } ),
+        
+        'colvis'
+    ],
     lengthMenu: [ [10, 25, 50, 100, -1], [10, 25, 50, 100, "todos"] ],
     stateSave: true,
     language: {
