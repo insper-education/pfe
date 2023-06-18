764236357f66adfa92b44d7ff6f4aad28a186561
Luciano Pereira Soares <lpsoares@gmail.com>
2023-06-18 20:16:01 -0300

    removendo migração

--- pfe/views.py
+++ pfe/views.py
@@ -23,36 +23,10 @@ def manutencao(request):
     """Página de Manutenção do Projeto Final de Engenharia."""
     return render(request, 'manutencao.html')
 
-from django.contrib.auth.models import Group
-from users.models import PFEUser
 
 @login_required
 @permission_required("users.view_administrador", raise_exception=True)
 def migracao(request):
     """temporário."""
-    message = "Feito"
-
-    adm_group = Group.objects.get(name="Administrador")
-    est_group = Group.objects.get(name="Estudante")
-    parc_group = Group.objects.get(name="Parceiro")
-    prof_group = Group.objects.get(name="Professor")
-
-    # TIPO_DE_USUARIO_CHOICES = (
-    #     (1, 'aluno'),
-    #     (2, 'professor'),
-    #     (3, 'parceiro'),
-    #     (4, 'administrador'),
-    # )
-
-    users = PFEUser.objects.all()
-    for user in users:
-        if user.tipo_de_usuario == 1:
-            user.groups.add(est_group)
-        elif user.tipo_de_usuario == 2:
-            user.groups.add(prof_group)
-        elif user.tipo_de_usuario == 3:
-            user.groups.add(parc_group)
-        elif user.tipo_de_usuario == 4:
-            user.groups.add(adm_group)
-
+    message = "Nada Feito"
     return HttpResponse(message)
