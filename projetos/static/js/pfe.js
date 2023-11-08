f837a9ad8c5c1d1e96900c91563e77e332bd6659
Luciano Pereira Soares <lpsoares@gmail.com>
2023-11-08 10:46:14 -0600

    melhor feedback de mensagem recebida

--- projetos/static/js/pfe.js
+++ projetos/static/js/pfe.js
@@ -6,3 +6,18 @@ function bars() {
     x.style.display = "block";
     }
 }
+
+// ------- RECURSO PARA DAR FEEDBACK   -------/////
+function pulsa_texto(atualizado, id) {
+    var f = document.getElementById(id);
+    f.style.color = "white";
+    if(atualizado) {
+        f.innerHTML = "Valores atualizados no servidor.";
+        f.style.backgroundColor = "green";
+    } else {
+        f.innerHTML = "Erro ao atualizar dados no servidor.";
+        f.style.backgroundColor = "red";
+    }
+    f.style.display = '';
+    setTimeout(function() {f.style.display = 'none';}, 2000);
+}
