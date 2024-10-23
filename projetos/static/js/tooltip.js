0bfd7bc1c3456505be11e9c00368ffc52e87bab0
Luciano Pereira Soares <lpsoares@gmail.com>
2024-10-23 20:50:41 -0300

    deixando tooltip na página base

---
+++ projetos/static/js/tooltip.js
@@ -0,0 +1,10 @@
+/*
+  Desenvolvido para o Projeto Final de Engenharia
+  Autor: Luciano Pereira Soares <lpsoares@insper.edu.br>
+  Data: 5 de Dezembro de 2020
+*/
+
+$(document).ready(function() {
+  $("[data-toggle='tooltip']").tooltip("dispose").tooltip({trigger : "hover", boundary: "window"});
+  //$("[data-toggle='tooltip']").tooltip("dispose").tooltip({trigger : "hover", boundary: "window", delay: { "show": 500, "hide": 1000 } });
+});
