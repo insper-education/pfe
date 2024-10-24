5ea5f4f9d3781b273a5b704270a618e3a8a9e46f
Luciano Pereira Soares <lpsoares@gmail.com>
2024-10-23 22:16:20 -0300

    melhorando organização da pagina base

--- projetos/static/js/pega_erro.js
+++ projetos/static/js/pega_erro.js
@@ -2,4 +2,24 @@
   Desenvolvido para o Projeto Final de Engenharia
   Autor: Luciano Pereira Soares <lpsoares@insper.edu.br>
   Data: 23 de Outubro de 2024
+  Referencia =  https://stackoverflow.com/questions/69470852/how-to-use-js-to-create-an-error-message-that-is-displayed-on-the-user-interface
 */
+
+window.onerror = function (msg, url, lineNo, columnNo, error) {
+  var string = String(msg).toLowerCase();
+  var substring = "script error";
+  if (string.indexOf(substring) > -1){
+    alert("Erro de Execução de JavaScript, contactar: lpsoares@insper.edu.br");
+  } else {
+    var message = [
+      "Message: " + msg,
+      "URL: " + url,
+      "Line: " + lineNo,
+      "Column: " + columnNo,
+      "Error object: " + JSON.stringify(error),
+      "contactar: lpsoares@insper.edu.br"
+    ].join(" - ");
+    alert(message);
+  }
+  return false;
+};
--- projetos/static/js/pfe.js
+++ projetos/static/js/pfe.js
@@ -1,18 +1,34 @@
-function bars() {
-    var myLinks = document.getElementById("myLinks");
-    if (myLinks.style.display === "block") {
-        myLinks.style.display = "none";
-    } else {
-        myLinks.style.display = "block";
-    }
-}
+/*
+  Desenvolvido para o Projeto Final de Engenharia
+  Autor: Luciano Pereira Soares <lpsoares@insper.edu.br>
+  Data: 23 de Outubro de 2024
+*/
 
-// Add event listener for the Escape key
+// estratégia para imprimir o título da página com underline e não espaço
+var tmp_titulo_pagina = document.title;
+window.addEventListener("beforeprint", function() {document.title = document.title.replace(/ /g, '_');});
+window.addEventListener("afterprint", function() {document.title = tmp_titulo_pagina;}); 
+
+// barra de menu lateral
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("hamburger").addEventListener("click", function(event) {
+        event.preventDefault();
+        var links_menu_lat = document.getElementById("links_menu_lat");
+        if (links_menu_lat.style.display === "block") {
+            links_menu_lat.style.display = "none";
+        } else {
+            links_menu_lat.style.display = "block";
+        }
+    });
+});
+
+
+// Trata quando a tecla Escape é pressionada
 document.addEventListener("keydown", function(event) {
     if (event.key === "Escape") {
-        var myLinks = document.getElementById("myLinks");
-        if (myLinks.style.display === "block") {
-            myLinks.style.display = "none";
+        var links_menu_lat = document.getElementById("links_menu_lat");
+        if (links_menu_lat.style.display === "block") {
+            links_menu_lat.style.display = "none";
         }
     }
 });
