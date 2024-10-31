20f6560c6d8dcdd348f2867a6e94eff3a4d81ecf
Luciano Pereira Soares <lpsoares@gmail.com>
2024-10-31 19:23:23 -0300

    escondendo o menu hambuguer ao clicar fora dele

--- projetos/static/js/pfe.js
+++ projetos/static/js/pfe.js
@@ -9,26 +9,31 @@ var tmp_titulo_pagina = document.title;
 window.addEventListener("beforeprint", function() {document.title = document.title.replace(/ /g, '_');});
 window.addEventListener("afterprint", function() {document.title = tmp_titulo_pagina;}); 
 
-// barra de menu lateral
 document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("hamburger").addEventListener("click", function(event) {
-        event.preventDefault();
-        var links_menu_lat = document.getElementById("links_menu_lat");
+    var links_menu_lat = document.getElementById("links_menu_lat");
+    var hamburger = document.getElementById("hamburger");
+    
+    // Clique na barra de menu lateral
+    hamburger.addEventListener("click", function(event) {
         if (links_menu_lat.style.display === "block") {
             links_menu_lat.style.display = "none";
         } else {
             links_menu_lat.style.display = "block";
         }
     });
-});
 
-
-// Trata quando a tecla Escape é pressionada
-document.addEventListener("keydown", function(event) {
-    if (event.key === "Escape") {
-        var links_menu_lat = document.getElementById("links_menu_lat");
-        if (links_menu_lat.style.display === "block") {
+    // Trata quando a tecla Escape é pressionada
+    document.addEventListener("keydown", function(event) {
+        if (event.key === "Escape" && links_menu_lat.style.display === "block") {
             links_menu_lat.style.display = "none";
         }
-    }
+    });
+
+    // Esconde o menu ao clicar fora dele
+    document.addEventListener("click", function(event) {
+        if (links_menu_lat.style.display === "block" && !links_menu_lat.contains(event.target) && !hamburger.contains(event.target)) {
+            links_menu_lat.style.display = "none";
+        }
+    });
+
 });
