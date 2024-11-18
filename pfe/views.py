743d5b115e1c142928d217ca3ea73cedae939f40
Luciano Pereira Soares <lpsoares@gmail.com>
2024-11-18 10:26:24 -0300

    migracao

--- pfe/views.py
+++ pfe/views.py
@@ -42,10 +42,26 @@ def custom_400(request, exception):
     #t.render(Context({"exception_value": value,})
     return HttpResponse(mensagem)
 
+
+from projetos.models import Configuracao, Organizacao, Proposta, Projeto
 @login_required
 @permission_required("users.view_administrador", raise_exception=True)
 def migracao(request):
     """temporário."""
-    message = "Nada Feito"
+    message = "Feito"
+
+    propostas = Proposta.objects.filter(disponivel=True)
+    for proposta in propostas:
+        proposta.habilidades = True
+        proposta.design = True
+        proposta.realistico = True
+        proposta.normas = True
+        proposta.restricoes = True
+        proposta.experimentacao = True
+        proposta.equipe = True
+        proposta.duracao = True
+        proposta.carga = True
+        proposta.mensuravel = True
+        proposta.save()
 
     return HttpResponse(message)
