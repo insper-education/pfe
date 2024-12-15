93184ff2dc0b6313beafa48f8dbefd0c43ac4c3a
Luciano Pereira Soares <lpsoares@gmail.com>
2024-12-15 11:49:49 -0300

    tentando evitar inconsistencia intermitente de versão carregada errada

--- templates/edicao_puxar_local_ou_versao.js
+++ templates/edicao_puxar_local_ou_versao.js
@@ -4,18 +4,20 @@
   Data: 21 de Fevereiro de 2024
 {% endcomment %}
 
-const itemStr = localStorage.getItem("filterEdicao");
-const item = JSON.parse(itemStr);
-const filterEdicao = item ? item.value : null;
-if (filterEdicao !== null && $("#filterEdicao option[value='"+filterEdicao+"']").length > 0 ) {
-  const prazo = 3600000; // 1 hora
-  const now = new Date().getTime();
-  // Verifica se não venceu
-  if (now > item.expiry + prazo) {
-    localStorage.removeItem("filterEdicao");
-  } else {
-    if(filterEdicao != "todas") { // Evita todas pois é muito lento
-      $("#filterEdicao").val(filterEdicao)
+$(document).ready(function() {
+  const itemStr = localStorage.getItem("filterEdicao");
+  const item = JSON.parse(itemStr);
+  const filterEdicao = item ? item.value : null;
+  if (filterEdicao !== null && $("#filterEdicao option[value='"+filterEdicao+"']").length > 0 ) {
+    const prazo = 3600000; // 1 hora
+    const now = new Date().getTime();
+    // Verifica se não venceu
+    if (now > item.expiry + prazo) {
+      localStorage.removeItem("filterEdicao");
+    } else {
+      if(filterEdicao != "todas") { // Evita todas pois é muito lento
+        $("#filterEdicao").val(filterEdicao)
+      }
     }
   }
-}
+});
