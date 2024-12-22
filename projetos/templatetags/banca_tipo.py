1f0fdafb41a855636ad3f4d883efb69b10c172ce
Luciano Pereira Soares <lpsoares@gmail.com>
2024-12-22 19:21:43 -0300

    removendo banca_tipo

--- projetos/templatetags/banca_tipo.py
+++ projetos/templatetags/banca_tipo.py
@@ -5,16 +5,19 @@ Autor: Luciano Pereira Soares <lpsoares@insper.edu.br>
 Data: 9 de Dezembro de 2020
 """
 
-from projetos.models import Banca
 
-from django import template
-register = template.Library()
+####  NAO MAIS USADO  #####
 
-@register.filter
-def get_tipo_banca(value):
-    """Retorna a string do tipo de banca."""
-    TIPOS = dict(Banca.TIPO_DE_BANCA)
-    if value in TIPOS:
-        return TIPOS[value]
-    else:
-        return "Tipo de banca não definido"
+# from projetos.models import Banca
+
+# from django import template
+# register = template.Library()
+
+# @register.filter
+# def get_tipo_banca(value):
+#     """Retorna a string do tipo de banca."""
+#     TIPOS = dict(Banca.TIPO_DE_BANCA)
+#     if value in TIPOS:
+#         return TIPOS[value]
+#     else:
+#         return "Tipo de banca não definido"
