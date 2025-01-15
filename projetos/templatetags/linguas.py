e615b2c75b3b4aa41355b2be0e804a8817e68b96
Luciano Pereira Soares <lpsoares@gmail.com>
2025-01-14 21:15:40 -0300

    sistema mais simples para multiidioma

--- projetos/templatetags/linguas.py
+++ projetos/templatetags/linguas.py
@@ -14,3 +14,31 @@ register = template.Library()
 @register.simple_tag
 def lng(pt_text, en_text):
     return mark_safe(f'''<span lang="pt">{pt_text}</span><span lang="en" style="display:none">{en_text}</span>''')
+
+# Quando a palavra muda por causa do gênero
+@register.simple_tag
+def lng_g(pt_genero, pt_text_m, pt_text_f, en_text_m, en_text_f=None):
+    if pt_genero.lower() == "m":
+        pt_text = pt_text_m
+        en_text = en_text_m
+    else:
+        pt_text = pt_text_f
+        if en_text_f is None:
+            en_text = en_text_m
+        else:
+            en_text = en_text_f
+    return mark_safe(f'''<span lang="pt">{pt_text}</span><span lang="en" style="display:none">{en_text}</span>''')
+
+# Quando a palavra muda por causa do número
+@register.simple_tag
+def lng_n(pt_numero, pt_text_s, pt_text_p, en_text_s, en_text_p=None):
+    if pt_numero <= 1:
+        pt_text = pt_text_s
+        en_text = en_text_s
+    else:
+        pt_text = pt_text_p
+        if en_text_p is None:
+            en_text = en_text_s
+        else:
+            en_text = en_text_p
+    return mark_safe(f'''<span lang="pt">{pt_text}</span><span lang="en" style="display:none">{en_text}</span>''')
