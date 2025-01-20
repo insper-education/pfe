d681a564ace3006a1b881178ddbca15d104644e7
Luciano Pereira Soares <lpsoares@gmail.com>
2025-01-20 16:51:32 -0300

    remove migração

--- pfe/views.py
+++ pfe/views.py
@@ -50,24 +50,10 @@ def custom_400(request, exception):
     #t.render(Context({"exception_value": value,})
     return HttpResponse(mensagem)
 
-
-from administracao.models import TipoCertificado, GrupoCertificado
-
 @login_required
 @permission_required("users.view_administrador", raise_exception=True)
 def migracao(request):
     """temporário."""
     message = "Nada Feito"
-    message = "Feito"
-
-    for grupo in GrupoCertificado.objects.all():
-        print(grupo)
-        print(grupo.nome)
-        print(grupo.sigla)
-        print(grupo.cor)
-        print(grupo.nome_en)
-        print(grupo.descricao)
-        print("")
-
     return HttpResponse(message)
 
