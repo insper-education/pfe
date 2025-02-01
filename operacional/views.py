a918cafbb6c72d026c9eefe75bcb7ed427c584b5
Luciano Pereira Soares <lpsoares@gmail.com>
2025-02-01 15:51:13 -0300

    pegando eventos certos

--- operacional/views.py
+++ operacional/views.py
@@ -336,7 +336,7 @@ def plano_aulas(request):
         if "edicao" in request.POST:
             ano, semestre = request.POST["edicao"].split('.')
             context = {
-                "aulas": Evento.get_evento(nome="Aula", ano=ano, semestre=semestre),
+                "aulas": Evento.get_eventos(nome="Aula", ano=ano, semestre=semestre),
                 "composicoes": filtra_composicoes(Composicao.objects.all(), ano, semestre),
                 }
             return render(request, "operacional/plano_aulas.html", context=context)
