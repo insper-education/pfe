965ff332e3ee234f7593ab97ecc3566d64ba61c6
Luciano Pereira Soares <lpsoares@gmail.com>
2025-08-13 17:17:57 -0300

    removendo bug de puxar indice 0 do hash

--- estudantes/views.py
+++ estudantes/views.py
@@ -508,8 +508,8 @@ def estudante_feedback_hashid(request, hashid):
     """Para Feedback finais dos Estudantes."""
     hashids = Hashids(salt=settings.SALT, min_length=8)
     try:
-        decoded = hashids.decode(hashid)[0]
-        if not decoded and len(decoded) > 1:
+        decoded = hashids.decode(hashid)
+        if not decoded:
             logger.warning(f"Hashid recebido é inválido: {hashid}")
             return HttpResponseNotFound("<h1>Usuário não encontrado! (hash inválido)</h1>")
         user_id = decoded[0]
