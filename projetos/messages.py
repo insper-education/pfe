7f806133beaeb38a4418b3cd5a980790a6ef04e5
Luciano Pereira Soares <lpsoares@gmail.com>
2025-09-04 11:38:14 -0300

    removento link que não faz sentido

--- projetos/messages.py
+++ projetos/messages.py
@@ -141,31 +141,32 @@ def prepara_mensagem_email(request, tipo, primarykey):
         }
         message = render_message("Mensagem Banca", context_carta)
 
-    if tipo == "banca_projeto":
+    #### NÃO PARECE FAZER SENTIDO ENVIAR MENSAGEM PARA UMA BANCA DE PROJETO SEM TER UMA BANCA ####
+    # if tipo == "banca_projeto":
     
-        projeto = get_object_or_404(Projeto, pk=primarykey)
-        banca = None
+    #     projeto = get_object_or_404(Projeto, pk=primarykey)
+    #     banca = None
 
-        para = ""
-        if banca.composicao.exame.sigla in ["BI", "BF"]:  # Interm ou Final
-            if projeto and projeto.orientador:
-                para += projeto.orientador.user.get_full_name() + " <" + projeto.orientador.user.email + ">; "
-                for coorientador in projeto.coorientador_set.all():
-                    para += coorientador.usuario.get_full_name() + " <" + coorientador.usuario.email + ">; "
-        if banca:
-            for membro in banca.membros():
-                para += membro.get_full_name() + " <" + membro.email + ">; "
+    #     para = ""
+    #     if banca.composicao.exame.sigla in ["BI", "BF"]:  # Interm ou Final
+    #         if projeto and projeto.orientador:
+    #             para += projeto.orientador.user.get_full_name() + " <" + projeto.orientador.user.email + ">; "
+    #             for coorientador in projeto.coorientador_set.all():
+    #                 para += coorientador.usuario.get_full_name() + " <" + coorientador.usuario.email + ">; "
+    #     if banca:
+    #         for membro in banca.membros():
+    #             para += membro.get_full_name() + " <" + membro.email + ">; "
         
-        if banca and banca.alocacao:
-            subject = "Capstone | Banca: " + banca.alocacao.aluno.user.get_full_name() + " [" + banca.alocacao.projeto.organizacao.nome + "] " +  banca.alocacao.projeto.get_titulo()
-        else:
-            subject = "Capstone | Banca: [" + projeto.organizacao.nome + "] " +  projeto.get_titulo()
+    #     if banca and banca.alocacao:
+    #         subject = "Capstone | Banca: " + banca.alocacao.aluno.user.get_full_name() + " [" + banca.alocacao.projeto.organizacao.nome + "] " +  banca.alocacao.projeto.get_titulo()
+    #     else:
+    #         subject = "Capstone | Banca: [" + projeto.organizacao.nome + "] " +  projeto.get_titulo()
         
-        context_carta = {
-            "request": request,
-            "projeto": projeto,
-        }
-        message = render_message("Mensagem Banca", context_carta)
+    #     context_carta = {
+    #         "request": request,
+    #         "projeto": projeto,
+    #     }
+    #     message = render_message("Mensagem Banca", context_carta)
 
     elif tipo == "certificado":
     
