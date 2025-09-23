3b5c55e2f3b4ecf9ca52ea5a7121e0b0af34ba2e
Luciano Pereira Soares <lpsoares@gmail.com>
2025-09-23 17:42:59 -0300

    ajustando exemplo

--- pfe/settings.example.py
+++ pfe/settings.example.py
@@ -108,6 +108,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'pfe.context_processors.navigation_menu',
             ],
         },
     },
