f836193c49487b3d72d73865837b3bebacbeafde
Luciano Pereira Soares <lpsoares@gmail.com>
2026-04-13 15:55:14 -0300

    mais informações dos links das mentorias

--- projetos/templatetags/bleach.py
+++ projetos/templatetags/bleach.py
@@ -6,14 +6,45 @@ Data: 31 de Outubro de 2024
 """
 
 import bleach
+import re
 from django import template
+from django.utils.safestring import mark_safe
+
 register = template.Library()
+
+ALLOWED_TAGS = ["a", "br", "p"]
+ALLOWED_ATTRS = {"a": ["href", "rel", "target", "title"]}
+ALLOWED_PROTOCOLS = ["http", "https", "mailto"]
+DISALLOWED_BLOCK_RE = re.compile(r"<(script|style)\b[^>]*>.*?</\1>", re.IGNORECASE | re.DOTALL)
+
+
+def _set_link_attributes(attrs, new=False):
+    rel_values = set(attrs.get((None, "rel"), "").split())
+    rel_values.update(["nofollow", "noopener", "noreferrer"])
+    attrs[(None, "rel")] = " ".join(sorted(rel_values))
+    href = attrs.get((None, "href"), "")
+    if href.startswith(("http://", "https://")):
+        attrs[(None, "target")] = "_blank"
+    return attrs
 
 @register.filter(name='bleach_urlize')
 def bleach_urlize(value):
-    # Tags permitidas
-    allowed_tags = ["a"]
-    allowed_attrs = {"a": ["href", "title", "target"]}
-    
-    # Linkify o texto
-    return bleach.linkify(value, parse_email=True, callbacks=[])
+    if not value:
+        return ""
+
+    text = DISALLOWED_BLOCK_RE.sub("", str(value))
+    sanitized = bleach.clean(
+        text,
+        tags=ALLOWED_TAGS,
+        attributes=ALLOWED_ATTRS,
+        protocols=ALLOWED_PROTOCOLS,
+        strip=True,
+    )
+    linkified = bleach.linkify(
+        sanitized,
+        parse_email=True,
+        callbacks=[_set_link_attributes],
+    )
+
+    return mark_safe(linkified)
+
